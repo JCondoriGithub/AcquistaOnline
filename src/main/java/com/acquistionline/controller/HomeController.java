25dde53156f562803fda2a9d4be776990cf118ca
jmcq <Jcondori_0096@hotmail.com>
2023-06-15 12:02:04 +0200

    implementazione: delle pagine grafiche index.html, home.html e dei controller che restituiscono le pagine

---
+++ src/main/java/com/acquistionline/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.acquistionline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
