4c1d072d3286eefee5f1c693cb34d05040691b15
jmcq <Jcondori_0096@hotmail.com>
2023-06-16 17:45:45 +0200

    implementata la funzionalità di poter registrare un nuovo cliente

--- src/main/resources/static/js/createClient.js
+++ src/main/resources/static/js/createClient.js
@@ -1,1 +1,32 @@
-console.log('ok');
+document.getElementById('btnCreateClient').addEventListener('click', createClient);
+
+function createClient() {
+
+    const name = document.getElementById('inputName').value;
+    const surname = document.getElementById('inputSurname').value;
+    const code = document.getElementById('inputCode').value;
+    const email = document.getElementById('inputEmail').value;
+
+    class Client {
+
+        constructor(name, surname, code, email) {
+            this.name = name;
+            this.surname = surname;
+            this.code = code;
+            this.email = email;
+        }
+    }
+
+    const newClient = new Client(name, surname, code, email);
+    const jsonClient = JSON.stringify(newClient);
+
+    fetch('api/clients', {
+        method: 'POST', headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }, body: jsonClient
+    }).then(res => {
+        if(res.status == 201)
+            alert('ora sei registrato!');
+    })
+}
